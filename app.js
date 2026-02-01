7346be37336164a0a4cba099cd3840e7ace0380f
Vernon Gibbs <vernon.gibbs@gmail.com>
2026-02-01 18:46:05 -0500

    Add chunked Google Docs ingest (character-budgeted, no export)

--- app.js
+++ app.js
@@ -73,6 +73,7 @@ app.use(require("./routes/google_oauth"));
 app.use(require("./routes/google_docs_import"));
 app.use(require("./routes/google_docs_view"));
 app.use(require("./routes/google_docs_ingest"));
+app.use(require("./routes/google_docs_ingest_chunked"));
 
 // ------------------------------------------------------------------
 // Export app + boot-time A0 validation hook (used by server.js)
